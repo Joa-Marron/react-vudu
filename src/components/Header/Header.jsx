0e39ed1a8b7b96a1170c57993e9a534b5042385a
Joa Marron <marronjoana@gmail.com>
2022-11-12 21:16:14 -0300

    Segunda preEntrega 2.0

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,8 +6,8 @@ import { Link } from 'react-router-dom'
 const Header = () => {
     return (
         <div>
-            <div className='logo'>
-            <Link to='/'><img src={logo} alt="" /></Link>
+            <div className='positionLogo'>
+            <Link to='/'><img src={logo} alt="" className='logo'/></Link>
             </div>
         </div >
     )
